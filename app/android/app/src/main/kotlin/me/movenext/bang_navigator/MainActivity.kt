213a239a4d196bf055a8edc09df6fa6f33d0cb50
Fabian Freund <github@fabi.online>
2025-05-05 14:28:57 +0200

    use engine caching

--- app/android/app/src/main/kotlin/me/movenext/bang_navigator/MainActivity.kt
+++ app/android/app/src/main/kotlin/me/movenext/bang_navigator/MainActivity.kt
@@ -1,7 +1,10 @@
 package me.movenext.bang_navigator
 
+import android.content.Context
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterFragmentActivity() {
@@ -10,11 +13,20 @@ class MainActivity: FlutterFragmentActivity() {
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+
         channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
     }
 
     override fun onTrimMemory(level: Int) {
         super.onTrimMemory(level)
         channel.invokeMethod("onTrimMemory", level)
+    }
+
+    override fun getCachedEngineId(): String {
+        return "engine_id"
+    }
+
+    override fun shouldDestroyEngineWithHost(): Boolean {
+        return false // Keep engine alive when activity is destroyed
     }
 }
