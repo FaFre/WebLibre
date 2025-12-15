e7a9d03bbccc786a728cebcd8e4c1eb9348c5c06
Fabian Freund <github@fabi.online>
2025-12-15 20:51:31 +0100

    upgrade deps

--- app/lib/features/bangs/domain/repositories/sync.dart
+++ app/lib/features/bangs/domain/repositories/sync.dart
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-import 'package:drift/isolate.dart';
 import 'package:exceptions/exceptions.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:weblibre/features/bangs/data/database/database.dart';
